40ff1061f3cbbdd67ae535bdec880e2f3d78b2b5
helloguy123ff <sadaraujo@gmail.com>
2024-06-11 14:58:41 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const virusContainer = document.getElementById("virus-container");
+    const virus = document.getElementById("virus");
+    const messages = [
+        "Haha! Gotcha!",
+        "I'm a funny virus!",
+        "Just kidding!",
+        "Don't worry, I'm harmless!",
+        "Got you again!",
+        "Boo!",
+    ];
+
+    const moveVirus = () => {
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+
+        const x = Math.random() * (screenWidth - virusContainer.offsetWidth);
+        const y = Math.random() * (screenHeight - virusContainer.offsetHeight);
+
+        virusContainer.style.transform = `translate(${x}px, ${y}px)`;
+
+        if (Math.random() < 0.3) {
+            createPopup(x, y);
+        }
+    };
+
+    const createPopup = (x, y) => {
+        const popup = document.createElement("div");
+        popup.className = "pop-up";
+        popup.style.left = `${x}px`;
+        popup.style.top = `${y}px`;
+        popup.innerText = getRandomMessage();
+        document.body.appendChild(popup);
+
+        setTimeout(() => {
+            document.body.removeChild(popup);
+        }, 3000);
+    };
+
+    const getRandomMessage = () => {
+        return messages[Math.floor(Math.random() * messages.length)];
+    };
+
+    setInterval(moveVirus, 2000);
+
+    virusContainer.addEventListener("mouseover", () => {
+        moveVirus();
+    });
+});
